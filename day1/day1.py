66da9a7eb74d1e8f7f68329d4d34fac8cda13620
Bren Moushall <bmoush@gmail.com>
2021-12-04 22:52:54 +1100

    day1part2 python

--- day1/day1.py
+++ day1/day1.py
@@ -13,3 +13,24 @@ for i, n in enumerate(numbers):
     elif numbers[i + 1] - n > 0:
         count += 1
 print(count)
+
+prev = None
+count = 0
+for i in range(len(numbers)):
+    s = sum(numbers[i: i + 3])
+    if prev is not None and s > prev:
+        count += 1
+    prev = s
+print(count)
+
+count = 0
+for i in range(len(numbers)):
+    try:
+        if numbers[i + 3] > numbers[i]:
+            count += 1
+    except IndexError:
+        continue
+print(count)
+
+
+print(len([n for i, n in enumerate(numbers) if i < len(numbers) - 3 and numbers[i + 3] > numbers[i]]))
