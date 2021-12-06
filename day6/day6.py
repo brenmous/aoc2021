33b493cc68a707873119cf9d20764085ff0ec7b2
Bren Moushall <bmoush@gmail.com>
2021-12-06 23:31:10 +1100

    day 6 part 2

--- day6/day6.py
+++ day6/day6.py
@@ -1,22 +1,26 @@
-
-def simulate(current_state):
-    new_state = []
-    for fish in current_state:
-        if fish == 0:
-            new_state.append(6)
-            new_state.append(8)
-        else:
-            new_state.append(fish - 1)
-    return new_state
-
+from collections import defaultdict
+from functools import lru_cache
 
 with open("input.txt", "r") as f:
-    state = [int(x) for x in f.read().split(',')]
-cycles = 80
+    fishes = [int(x) for x in f.read().split(',')]
 
 
-for day in range(cycles):
-    # print(f"Day {day}: {state}")
-    state = simulate(state)
-print(len(state))
+@lru_cache
+def simulate(fish, day):
+    if day == 0:
+        return 1
+    elif fish == 0:
+        return simulate(6, day - 1) + simulate(8, day - 1)
+    else:
+        return simulate(fish - 1, day - 1)
 
+
+cycles = 256
+fish_count = defaultdict(int)
+for fish in fishes:
+    fish_count[fish] += 1
+unique_fish = list(set(fishes))
+total = []
+for fish in unique_fish:
+    total.append(simulate(fish, cycles) * fish_count[fish])
+print(sum(total))
