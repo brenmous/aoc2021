2386db333a36a714b8e74900838187c3d610c721
Bren Moushall <bmoush@gmail.com>
2021-12-07 23:32:49 +1100

    day 7 parts 1 and 2 clojure

---
+++ day7/day7.clj
@@ -0,0 +1,37 @@
+(defn get_crabs [file]
+  (map read-string (clojure.string/split (slurp file) #","))
+)
+
+(defn abs [n] (max n (- n)))
+
+(defn moves [end]
+  (if (<= end 1) end (+ end (moves (- end 1))))
+)
+
+(defn moves2 [end]
+  (reduce + (range 1 (+ 1 end)))
+)
+
+(defn fuel_costing1 [crab pos]
+  (abs (- crab pos))
+)
+
+(defn fuel_costing2 [crab pos]
+  (moves2 (abs(- crab pos)))
+)
+  
+(defn part1 [crabs]
+  (for [pos (range 1 (+ (apply max crabs) 1))]
+    (reduce + (map (fn [crab] (fuel_costing1 crab pos)) crabs))
+  )
+)
+
+(defn part2 [crabs]
+  (for [pos (range 1 (+ (apply max crabs) 1))]
+    (reduce + (map (fn [crab] (fuel_costing2 crab pos)) crabs))
+  )
+)
+
+(def crabs (get_crabs "input.txt"))
+(println (apply min (part1 crabs)))
+(println (apply min (part2 crabs)))
