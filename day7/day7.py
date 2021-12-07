b1082ac8fd437347b9fee186574ffc17e95619d7
Bren Moushall <bmoush@gmail.com>
2021-12-07 22:50:52 +1100

    day 7 part 2
    can't even do the stupid way smart, how embarrassing

--- day7/day7.py
+++ day7/day7.py
@@ -1,35 +1,50 @@
 from collections import defaultdict
+import sys
 
-with open("test_input.txt", "r") as f:
+with open("input.txt", "r") as f:
     crabs = [int(x) for x in f.read().split(',')]
 
 def brute(crabs):
     num_crabs = defaultdict(int)
     for crab in crabs:
         num_crabs[crab] += 1
-    fuel_costs = defaultdict(int)
     possible_positions = range(1, max(crabs) + 1)
     unique_crabs = sorted(list(set(crabs)))
+    best_cost = sys.maxsize
+    best_pos = 0
     for pos in possible_positions:
         fuel = 0
         for crab in unique_crabs:
             fuel += abs(crab - pos) * num_crabs[crab]
-        fuel_costs[pos] += fuel
-    return fuel_costs
+            if fuel >= best_cost:
+                break
+        if fuel < best_cost:
+            best_cost = fuel
+            best_pos = pos
+
+    return best_pos, best_cost
+
+print(brute(crabs))
 
 def brute_2(crabs):
     num_crabs = defaultdict(int)
     for crab in crabs:
         num_crabs[crab] += 1
-    fuel_costs = defaultdict(int)
     possible_positions = range(1, max(crabs) + 1)
     unique_crabs = sorted(list(set(crabs)))
+    best_cost = sys.maxsize
+    best_pos = 0
     for pos in possible_positions:
         fuel = 0
         for crab in unique_crabs:
             moves = range(1, abs(crab - pos) + 1)
             fuel += sum(moves) * num_crabs[crab]
-        fuel_costs[pos] += fuel
-    return fuel_costs
+            if fuel >= best_cost:
+                break
+        if fuel < best_cost:
+            best_cost = fuel
+            best_pos = pos
 
-print(min(brute_2(crabs).values()))
+    return best_pos, best_cost
+
+print(brute_2(crabs))
