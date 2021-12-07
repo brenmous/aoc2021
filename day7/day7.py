f34b7a163069178ecb33477694a9a076cf77c4f6
Bren Moushall <bmoush@gmail.com>
2021-12-07 22:28:37 +1100

    day 7 part 2 python

--- day7/day7.py
+++ day7/day7.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-with open("input.txt", "r") as f:
+with open("test_input.txt", "r") as f:
     crabs = [int(x) for x in f.read().split(',')]
 
 def brute(crabs):
@@ -17,4 +17,19 @@ def brute(crabs):
         fuel_costs[pos] += fuel
     return fuel_costs
 
-print(min(brute(crabs).values()))
+def brute_2(crabs):
+    num_crabs = defaultdict(int)
+    for crab in crabs:
+        num_crabs[crab] += 1
+    fuel_costs = defaultdict(int)
+    possible_positions = range(1, max(crabs) + 1)
+    unique_crabs = sorted(list(set(crabs)))
+    for pos in possible_positions:
+        fuel = 0
+        for crab in unique_crabs:
+            moves = range(1, abs(crab - pos) + 1)
+            fuel += sum(moves) * num_crabs[crab]
+        fuel_costs[pos] += fuel
+    return fuel_costs
+
+print(min(brute_2(crabs).values()))
